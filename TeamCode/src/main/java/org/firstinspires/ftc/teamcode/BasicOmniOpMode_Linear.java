ad6a52a0e31752aaefc85ec5e652b9bc7a7f5848
Masterpro15 <evanckurian@gmail.com>
2024-11-03 20:22:55 -0600

    Adding viper slide code- Test 3 test2= working, increase angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -114,7 +114,7 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
     final double ARM_COLLECT               = 10 * ARM_TICKS_PER_DEGREE;
     final double ARM_CLEAR_BARRIER         = 15 * ARM_TICKS_PER_DEGREE;
     final double ARM_SCORE_SPECIMEN        = 60 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW   = 60 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW   = 80 * ARM_TICKS_PER_DEGREE;
     final double ARM_ATTACH_HANGING_HOOK   = 0 * ARM_TICKS_PER_DEGREE;
     final double ARM_WINCH_ROBOT           = 10  * ARM_TICKS_PER_DEGREE;
 
@@ -124,7 +124,7 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
     final double INTAKE_DEPOSIT    =  0.5;
 
     /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
-    final double WRIST_FOLDED_IN   = 1;
+    final double WRIST_FOLDED_IN   = 0.1667;
     final double WRIST_FOLDED_OUT  = 0.5;
     final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
 
