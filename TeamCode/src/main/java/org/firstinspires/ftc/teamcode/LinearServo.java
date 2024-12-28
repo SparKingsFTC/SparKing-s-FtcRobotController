d9713c7cfc9a7734239876e806b53372454b7b07
WingedLeopard <therealfakeanthony@gmail.com>
2024-12-27 18:51:35 -0600

    auto encoder thing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearServo.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@TeleOp(name="Linear Servo Testing", group="Linear OpMode")
+public class LinearServo extends LinearOpMode {
+    static final double MAX_POS = 0.9;
+    static final double MIN_POS = 0.1;
+    ServoImplEx servo;
+    @Override
+    public void runOpMode() {
+        servo = hardwareMap.get(ServoImplEx.class, "");
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()){
+            servo.setPosition(MAX_POS);
+            sleep(4000);
+            servo.setPosition(MIN_POS);
+            sleep(4000);
+        }
+    }
+}
