38684c1559c5fb5c030b3bfda579085a329aae85
Masterpro15 <evanckurian@gmail.com>
2025-01-10 19:19:31 -0600

    auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
@@ -180,11 +180,11 @@ public class AutoSpecimenMiddle extends LinearOpMode {
         ForwardBackward(0.75, 15, 1);
         armMotorPlacement(0.5, ARM_COLLAPSED_INTO_ROBOT);
         //turnLeft(0.7, 1370);
-        turnLeft(0.7, 1440);
+        turnLeft(0.7, 1420);
         Right(0.75, 43);
         wrist.setPosition(0.5);
         sleep(500);
-        ForwardBackward(0.5, 1, 1);
+        ForwardBackward(0.5, 1.5,  1);
         claw.setPosition(1);
         sleep(1000);
         armMotorPlacement(0.75, ARM_SCORE_SPECIMEN2);
@@ -195,10 +195,9 @@ public class AutoSpecimenMiddle extends LinearOpMode {
         ForwardBackward(0.5, 18, -1);
         armMotorPlacement(0.5, ARM_SCORE_SPECIMEN);
         claw.setPosition(1);
-        ForwardBackward(0.5,1, -1);
-        wrist.setPosition(0.9);
         wrist.setPosition(0.67);
-        ForwardBackward(1,4.5, -1);
+        wrist.setPosition(0.9);
+        ForwardBackward(1,3, -1);
         armMotorPlacement(0.5, ARM_COLLAPSED_INTO_ROBOT);
         wrist.setPosition(0);
         ForwardBackward(0.75, 17.5, 1);
