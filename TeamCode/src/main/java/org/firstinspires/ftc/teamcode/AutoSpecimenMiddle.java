35052070eb0fc7a781b45f77cbe09bcb8f676931
Masterpro15 <evanckurian@gmail.com>
2025-01-17 18:31:27 -0600

    weird claw stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
@@ -185,7 +185,7 @@ public class AutoSpecimenMiddle extends LinearOpMode {
         wrist.setPosition(0.5);
         sleep(500);
         ForwardBackward(0.5, 1,  -1);
-        ForwardBackward(0.5, 0.4,  1);
+        ForwardBackward(0.5, 0.4,  -1);
         claw.setPosition(1);
         sleep(1000);
         armMotorPlacement(0.75, ARM_SCORE_SPECIMEN2);
