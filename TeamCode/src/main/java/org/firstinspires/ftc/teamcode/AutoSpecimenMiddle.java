dcf5bc9d5ba0b7945f6b171581dcbff5d3e65e70
Masterpro15 <evanckurian@gmail.com>
2025-01-17 22:48:29 -0600

    robot broke but fixedddd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpecimenMiddle.java
@@ -184,7 +184,7 @@ public class AutoSpecimenMiddle extends LinearOpMode {
         Right(0.75, 43);
         wrist.setPosition(0.5);
         sleep(500);
-        ForwardBackward(0.5, 2.2,  -1);
+        ForwardBackward(0.5, 4,  -1);
         ForwardBackward(0.5, 0.2,  -1);
         claw.setPosition(1);
         sleep(1000);
